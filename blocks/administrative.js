1b1ec82c2cc5c645e76f9497e7663b007387de06
daarond <daarond@gmail.com>
2016-06-26 12:35:18 -0500

    adding administrative blocks

---
+++ blocks/administrative.js
@@ -0,0 +1,51 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.Administrative');
+
+goog.require('Blockly.Blocks');
+
+
+/**
+ * Common HSV hue for all blocks in this category.
+ */
+Blockly.Blocks.colour.HUE = 20;
+
+Blockly.Blocks['administrative_notify'] = {
+    init: function() {
+        this.appendValueInput("NAME")
+            .setCheck("String")
+            .appendField("notify me, message");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(Blockly.Blocks.colour.HUE);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['administrative_log_message'] = {
+    init: function() {
+        this.appendValueInput("NAME")
+            .setCheck("String")
+            .appendField("save log message");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(Blockly.Blocks.colour.HUE);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.Blocks['administrative_result'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("write")
+            .appendField(new Blockly.FieldDropdown([["success", "SUCCESS"], ["failure", "FAILURE"]]), "RESULT")
+            .appendField("result");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(20);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
